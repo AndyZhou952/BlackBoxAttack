2f31d7ffb8fb15f0659d3c54103df7c9b5261948
chongkwongyu <chongkwongyu2@gmail.com>
2023-12-04 19:19:55 -0500

    pretrain densenet

--- model/butterfly_classifier.py
+++ model/butterfly_classifier.py
@@ -42,4 +42,21 @@ class Classifier(torch.nn.Module):
         x = x.view(x.size()[0], -1)
         logits = self.fc(x)
         return logits
-    
+
+# Densenet121
+class DenseNet121(nn.Module):
+
+    def __init__(self, num_classes, model='densenet121', pretrained=True):
+        super().__init__()
+        self.model = timm.create_model(model, pretrained, in_chans=3)
+        self.fc1 = nn.Linear(1000,32)
+        self.fc2 = nn.Linear(32,64)        
+        self.fc3 = nn.Linear(64,num_classes)
+
+        
+    def forward(self, x):
+        x = self.model(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        return x
