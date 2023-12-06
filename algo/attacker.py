d90a0fb3be8aeebfb47d733f439349b5c9ba18b7
remote233 <yyuan2696@gmail.com>
2023-12-05 20:25:43 -0500

    PIA update

--- algo/attacker.py
+++ algo/attacker.py
@@ -36,15 +36,7 @@ def adversarial_generator(model, target_class, image, search_var, sample_num, bo
     return adv_image
 
 # PIA algo
-def is_target_in_top_k(model, image, target_class, k=5):
-    
-    with torch.no_grad():
-        output = model(image.unsqueeze(0))
-    probabilities = F.softmax(output, dim=1)
-    top_probs, top_classes = torch.topk(probabilities, k)
-    return target_class in top_classes[0]
-
-def PIA_adversarial_generator(model, initial_image, image, target_class, epsilon_adv, epsilon_0, search_var, sample_num, delta_epsilon, eta_max, eta_min, query_limit):
+def PIA_adversarial_generator(model, initial_image, image, target_class, epsilon_adv, epsilon_0, search_var, sample_num, delta_epsilon, eta_max, eta_min, k=5):
     device = next(model.parameters()).device
     initial_image, image = initial_image.to(device), image.to(device)
     x_adv = image.clone().to(device)
@@ -57,12 +49,14 @@ def PIA_adversarial_generator(model, initial_image, image, target_class, epsilon
     original_class = torch.argmax(model(initial_image), dim=1)
     
     with torch.no_grad():
+        probabilities_adv = F.softmax(model(x_adv), dim=1)
+        top_probs, top_classes = torch.topk(probabilities, k)
         while (epsilon > epsilon_adv) | (original_class != target_class):
             gradient = NES(model, target_class, x_adv, search_var, sample_num, g, u)
             eta = eta_max
-            x_adv_hat = x_adv - eta * gradient.sign()
+            x_adv_hat = x_adv - eta * gradient
             
-            while not is_target_in_top_k(model, x_adv_hat, target_class, k):
+            while not target_class in top_classes[0]:
                 if eta < eta_min:
                     epsilon += delta_epsilon
                     delta_epsilon /= 2
