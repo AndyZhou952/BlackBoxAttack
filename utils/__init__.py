8cf7a6c9ff012bf1fde189a1a3d3a4e3589b3882
AndyZhou952 <jzhoubc@connect.ust.hk>
2023-12-07 11:40:14 -0500

    util.base get correct subset

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,6 +2,6 @@ import torch
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from PIL import Image
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, Subset
 import random
 import os
--- utils/base.py
+++ utils/base.py
@@ -1,5 +1,5 @@
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 
 def train_classifier(model, train_loader, test_loader, optimizer, criterion, epoch):
     device = next(model.parameters()).device
@@ -44,3 +44,29 @@ def eval_accuracy(model, dataset, batch_size=100):
             count += (output == label).sum()
     accuracy = count / len(dataset)
     return accuracy
+
+def get_correct_predictions_subset(model, dataset, batch_size=100):
+    device = next(model.parameters()).device
+    model.eval()
+
+    dataloader = DataLoader(dataset=dataset, batch_size=batch_size)
+
+    correct_indices = []
+    count = 0
+    with torch.no_grad():
+        for batch_idx, (inp, label) in enumerate(dataloader):
+            inp, label = inp.to(device), label.to(device)
+            output = model(inp)
+            pred = torch.argmax(output, dim=1)
+            correct = (pred == label)
+            count += correct.sum()
+
+            # Add indices of correct predictions to the list
+            if correct.any():
+                indices = batch_idx * batch_size + torch.where(correct)[0]
+                correct_indices.extend(indices.cpu().numpy())
+
+    accuracy = count / len(dataset)
+    correct_subset = Subset(dataset, correct_indices)
+
+    return accuracy, correct_subset
