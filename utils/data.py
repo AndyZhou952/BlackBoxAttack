f28e316846553fa09eab4f81adbdd563ee31014c
AndyZhou952 <jzhoubc@connect.ust.hk>
2023-12-08 00:43:50 -0500

    imagenet reader

--- utils/data.py
+++ utils/data.py
@@ -2,7 +2,7 @@ import torch
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from PIL import Image
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
 import random
 import os
 
@@ -77,4 +77,48 @@ def create_butterfly_dataset(path = './data/butterfly/', img_reshape=(3, 224, 22
     trainset = ImageDataset(paths0, transform)
     testset = ImageDataset(tpaths0, transform)
     
+    return trainset, testset, normal_mapping, reverse_mapping, sample_img_dataset
+
+def create_imagenet_dataset(path='./data/tiny_imagenet/tiny-imagenet-200/', img_reshape=(3, 224, 224), split_ratio = 0.8):
+    C, H, W = img_reshape
+    transform = transforms.Compose([
+                                    transforms.Resize((H, W)),
+                                    transforms.ToTensor()])
+      
+    data_path = path + 'train'
+    
+    class_names = sorted(os.listdir(data_path))
+    N = list(range(len(class_names)))
+    normal_mapping = dict(zip(class_names, N)) 
+    reverse_mapping = dict(zip(N, class_names))
+
+    paths0 = []
+    sample_img_dataset = torch.zeros((len(class_names), C, H, W))
+    seen = list()
+
+    for label in class_names:
+        label_path = os.path.join(data_path, label, 'images')
+        if not os.path.isdir(label_path):
+            continue
+        for filename in os.listdir(label_path):
+            if filename.endswith('.JPEG'):
+                path = os.path.join(label_path, filename)
+                paths0.append((path, normal_mapping[label]))
+                if label not in seen:
+                    image = Image.open(path).convert('RGB')
+                    image = transform(image)
+                    sample_img_dataset[normal_mapping[label], :, :, :] = image
+                    seen.append(label)
+                
+    random.seed(123)
+    random.shuffle(paths0) 
+    
+    data = ImageDataset(paths0, transform)
+
+    total_size = len(data)
+    train_size = int(split_ratio * total_size)
+    test_size = total_size - train_size
+
+    trainset, testset = random_split(data, [train_size, test_size])
+
     return trainset, testset, normal_mapping, reverse_mapping, sample_img_dataset
